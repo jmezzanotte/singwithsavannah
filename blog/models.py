80841bedb074a4ecca0c4649b0020b1e92ff5727
rob pennock <robpennock@gmail.com>
2016-12-05 22:09:56 -0800

    merged this bitch with new master

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
+class BlogPost(models.Model):
+	usr = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=250)
+	blog_text = models.TextField(max_length=10000)
+	date_created = models.DateTimeField(auto_now_add=True)
