01ae63e78cb2adfaed01df75e28d022c296e5de0
jmezzanotte <johnjmezzanotte@gmail.com>
2016-12-06 00:17:08 -0800

    Started playing arround with database models

--- sws_site/models.py
+++ sws_site/models.py
@@ -16,6 +16,7 @@ class ServicesLandingPage(models.Model):
 			entries but also be able only to use the newest ones. 
 			Specifying get latest by will allow us to use only the 
 			latest record later on. 
+			
 		'''
 		verbose_name = "Services Landing Page"
 		verbose_name_plural="Services Landing Page"
