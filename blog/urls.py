db5d920e3fe1e62f881578767b1fa79a4242d46b
rob pennock <robpennock@gmail.com>
2016-12-08 12:48:50 -0800

    pushing recent merge with blog to that routes specific blog ids for blog show functionality

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 	#singwithsavannah.com/music
     url(r'^$', views.index, name='index'),
 
-    #routes for blog show functionality
+    #routes for blog  show functionality
     #singwithsavannah.com/music/1234...n
     url(r'^(?P<blogpost_id>[0-9]+)/$', views.show, name='show'),
 ]
