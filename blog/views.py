c7145edbdcc979ac4e7d316b78975684eff2eb23
rob pennock <robpennock@gmail.com>
2016-12-08 12:47:27 -0800

    Added routes to blog/urls.py to allow for specific blog post items to be shown. For example singwithsavannah.com/blog/3

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import BlogPost
 # Create your views here.
 def index(request):
-	return HttpResponse("<h1>This is the blog index page</h2>")
+	all_blog_posts = BlogPost.objects.all()
+	html = ''
+	for blog_post in all_blog_posts:
+		url='/blog/' + str(blog_post.id)
+		html += '<a href="' + url + '"> '+ blog_post.title +'</a>'
+	return HttpResponse(html)
+
+def show(request, blogpost_id):
+	return HttpResponse("<h1>This page is to check blog_post link to database. id: " + str(blogpost_id) + "</h1>")
