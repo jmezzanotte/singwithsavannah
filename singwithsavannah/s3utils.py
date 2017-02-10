a69f9fba21661ff3a3ccb914edd69c3e372c6332
jmezzanotte <johnjmezzanotte@gmail.com>
2017-02-09 23:24:16 -0800

    Fixed name of class in s3utils.py

--- singwithsavannah/s3utils.py
+++ singwithsavannah/s3utils.py
@@ -1,5 +1,5 @@
 from storages.backends.s3boto import S3BotoStorage
 
 StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
-MediaRootBotoStorage = lambda: S3BotoStorage(location='media')
+MediaRootS3BotoStorage = lambda: S3BotoStorage(location='media')
 
