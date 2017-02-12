dc9aacfe0cf57b8d67a04d18b4c337e7f71ab854
jmezzanotte <johnjmezzanotte@gmail.com>
2017-02-11 23:38:22 -0800

    taking out underscores in the filename template tag

--- sws_site/templatetags/filename.py
+++ sws_site/templatetags/filename.py
@@ -7,6 +7,6 @@ register = template.Library()
 @register.filter(name='filename')
 def filename(value):
 	'''Custom filter tag to remove the file name'''
-	return os.path.basename(os.path.splitext(value)[0])
+	return os.path.basename(os.path.splitext(value)[0]).replace('_', '')
 
 #register.filter('filename', filename)
