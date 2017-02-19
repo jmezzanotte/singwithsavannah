eecd0ed9cd8f41740c0b0b262c681f12ed9fb381
jmezzanotte <johnjmezzanotte@gmail.com>
2017-02-19 11:21:34 -0800

    Added a now playing feature to the audio player.

--- static/js/music.js
+++ static/js/music.js
@@ -1,5 +1,9 @@
 (function($){
 
+	String.prototype.toProperCase = function () {
+    	return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	};
+
 	//var audio = $('.audio-player')[0];	
 	var audioExt = '.mp3'; 
 	var isPlayer = false;
@@ -12,7 +16,14 @@
 		'use strict'; 
 		// This line will get you to the appropriate audio player
 		//console.log($(e.target).parent().prev()[0]);
-		var audio = $(e.target).parent().prev()[0];
+		// var audio = $(e.target).parent().prev()[0];
+		var audio = null;
+		console.log($($(e.target).parent().prev()[0]).is('audio'));
+		if($($(e.target).parent().prev()[0]).is('audio')){
+			audio = $(e.target).parent().prev()[0];
+		}else{
+			audio = $(e.target).parent().prev().prev()[0];
+		}
 
 		if(currentSongObj){
 			currentSongObj.removeClass('active-song');
@@ -21,24 +32,28 @@
 		if(currentAudioPlayer){
 			currentAudioPlayer.pause();
 		}
-		if(currentSong != e.target.id) {
 
+		if(currentSong != e.target.id) {
 			var audioURL = $(e.target).find('.hidden').text();
 			audio.src = audioURL;
 			audio.play();
+			var nowPlaying = 'Now playing: ' + e.target.id;
+			nowPlaying = nowPlaying.replace(/-/gi, ' ').toProperCase();
 			currentSong = e.target.id;
 			$(e.target).addClass('active-song');
+			$($(audio).next()[0]).text(''); 
+			$($(audio).next()[0]).text(nowPlaying);
 
 		}else{
 			audio.pause();
 			currentSong = '';
 			currentAudioPlayer = null;
-			console.log("Current Song" + currentSong);
-			console.log(currentSongObj);
 		}
-
+		
 		currentSongObj = $(e.target);
 		currentAudioPlayer = audio;
+		
+		
 
 	});
 
