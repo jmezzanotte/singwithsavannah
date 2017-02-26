e7564b3fa26766e2247e9845c1095d46c4c5515e
jmezzanotte <johnjmezzanotte@gmail.com>
2017-02-25 21:47:05 -0800

    modified urls.py to use if Debug is false point static url to Static root

--- singwithsavannah/urls.py
+++ singwithsavannah/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
 
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
