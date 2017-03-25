5fbcc367c0fcf59287ead638c62751c34002b8c3
jmezzanotte <johnjmezzanotte@gmail.com>
2017-03-25 00:12:02 -0700

    Added a proxy header to see if it fixes my server errors

--- singwithsavannah/settings.py
+++ singwithsavannah/settings.py
@@ -27,6 +27,9 @@ SECRET_KEY = '(n&3hk9#qi5g!1aj!w-x=(2j$ylra_ugy1+@_@4htvg=ie4b$4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 ALLOWED_HOSTS = ['https://singwithsavannah.herokuapp.com/']
 
 # Application definition
