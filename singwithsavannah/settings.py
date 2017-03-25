8dd3899e5298e802e33fef461527817867952844
jmezzanotte <johnjmezzanotte@gmail.com>
2017-03-25 00:20:50 -0700

    Set use x forwarded host to true. Seeing if this fixes the issue

--- singwithsavannah/settings.py
+++ singwithsavannah/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = '(n&3hk9#qi5g!1aj!w-x=(2j$ylra_ugy1+@_@4htvg=ie4b$4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+USE_X_FORWARDED_HOST = True
 
 ALLOWED_HOSTS = ['.singwithsavannah.herokuapp']
 
