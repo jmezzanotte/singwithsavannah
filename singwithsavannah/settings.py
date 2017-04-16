f4980a39b26b9c563aed8cc4215823010f697635
jmezzanotte <johnjmezzanotte@gmail.com>
2017-04-15 17:38:48 -0700

    Fixed mobile landscape image.

--- singwithsavannah/settings.py
+++ singwithsavannah/settings.py
@@ -65,8 +65,8 @@ else:
 # Rememeber to unlock captcha
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'johnmezzportfolio@gmail.com'
+EMAIL_HOST_PASSWORD = 'prunePortfolio22'
 EMAIL_PORT = 587
 
 # Application definition
