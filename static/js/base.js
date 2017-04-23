ac6edb1136161ca4e4c0f87213d6cee59eafa2a8
jmezzanotte <johnjmezzanotte@gmail.com>
2017-04-23 13:24:42 -0700

    Switched back to flex-box layout

--- static/js/base.js
+++ static/js/base.js
@@ -6,9 +6,7 @@ $(function(){
 	$('#sws-cloud-mobi').show().animate({'top' : '40%', 'visibility' : 'visible'}, 1000);
 	$('#about-overlay').show().animate({'top' : '50%', 'visibility' : 'visible'}, 1000);
 	
-	// console.log(window.screen.height);
-	// console.log(window.innerHeight);
-	// console.log('brow')
+
 	// console.log(window.orientation);
 	// console.log(window.matchMedia("(orientation: portrait)").media);
 
@@ -28,8 +26,7 @@ $(function(){
 	//   	$('#sws-logo-mobi').show().animate({'top' : '60%', 'visibility' : 'visible'}, 1000);
 	//   }
 	// }, false);
-	// Sticky nav bar 
-
+	// Sticky nav bar  
 	
 	$('.secondary-topbanner').sticky({
 		topSpacing : 0, 
