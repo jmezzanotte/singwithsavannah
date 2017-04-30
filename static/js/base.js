7d89ba46a14c49fa7c360b5e18cce4d4d7063b72
jmezzanotte <johnjmezzanotte@gmail.com>
2017-04-29 22:09:27 -0700

    Changed style of navbar. Created a separate nav.css file to organize css.

--- static/js/base.js
+++ static/js/base.js
@@ -1,7 +1,7 @@
 $(function(){
 	
 	// onload animation
-	$('#sws-logo').show().animate({'top' : '70%', 'visibility' : 'visible'}, 1000);
+	$('#sws-logo').show().animate({'top' : '60%', 'visibility' : 'visible'}, 1000);
 	$('#sws-logo-mobi').show().animate({'top' : '60%', 'visibility' : 'visible'}, 1000);
 	$('#sws-cloud-mobi').show().animate({'top' : '40%', 'visibility' : 'visible'}, 1000);
 	$('#about-overlay').show().animate({'top' : '50%', 'visibility' : 'visible'}, 1000);
