24824071f0e5a2b08831a2cb1e128a61e1678247
jmezzanotte <johnjmezzanotte@gmail.com>
2017-08-26 23:41:23 -0700

    Added favicon path in urls.py

--- sws_site/urls.py
+++ sws_site/urls.py
@@ -1,3 +1,4 @@
+from django.views.generic.base import RedirectView
 from django.conf.urls import url
 from . import views
 
@@ -9,5 +10,6 @@ urlpatterns = [
     url(r'^services/$', views.services, name='services'),
     url(r'^contact', views.contact, name='contact'),
     url(r'^testimonies', views.testimonials, name='testimonies'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon/favicon.ico')),
     url(r'^services/(?P<slug>[\w-]+)/$', views.service_detail, name='service_detail')   
 ]
